5adb50e189ee050af80e47ac80cb77ee922c8e75
binilet <let_me_know47@yahoo.com>
2026-02-06 22:18:44 +0300

    testing ci flow

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -2,4 +2,4 @@ import pytest
 
 @pytest.mark.parametrize("x,y,expected", [(1, 1, 2)], ids=["CI sanity check"])
 def test_ci_sanity_pass(x, y, expected):
-    assert x + y == expected
+    assert x + y == expected 
